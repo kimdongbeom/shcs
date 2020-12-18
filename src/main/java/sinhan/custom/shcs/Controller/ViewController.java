82018b332339e52ecedbf4770ac4f74227e6ab3e
dongbeom-kim <dongbeom-kim@nhnent.com>
2020-12-18 09:58:37 +0900

    파일 삭제 및 업로드 관련 기능 작업

--- src/main/java/sinhan/custom/shcs/Controller/ViewController.java
+++ src/main/java/sinhan/custom/shcs/Controller/ViewController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,6 +19,7 @@ import sinhan.custom.shcs.model.ResultExcel;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,9 +38,7 @@ public class ViewController {
         File files[] = dir.listFiles();
         List<String> fileNames = new ArrayList<>();
 
-        for (File file : files) {
-            fileNames.add(file.getName());
-        }
+        exceptNotPdfFile(files, fileNames, ".pdf");
 
         model.addAttribute("service", param);
         model.addAttribute("fileNames", fileNames);
@@ -80,19 +78,61 @@ public class ViewController {
         return new ModelAndView(new LenovoExcelView());
     }
 
+    @PostMapping("/upload/pdf/da")
+    public String uploadDaPDF(Model model, @RequestParam("file") MultipartFile multipartFile) {
+        try {
+            multipartFile.transferTo(new File(daPdfUploadDir + multipartFile.getOriginalFilename()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File dir = new File(daPdfUploadDir);
+        File files[] = dir.listFiles();
+        List<String> fileNames = new ArrayList<>();
+
+        exceptNotPdfFile(files, fileNames, ".pdf");
+
+        model.addAttribute("fileNames", fileNames);
+        return "home ::#fileList";
+    }
+
     @GetMapping("/search/pdf")
     public String searchPdfFile(Model model, @RequestParam("query") String query) {
         File dir = new File(daPdfUploadDir);
         File files[] = dir.listFiles();
         List<String> fileNames = new ArrayList<>();
 
+        exceptNotPdfFile(files, fileNames, query);
+
+        model.addAttribute("fileNames", fileNames);
+        return "home ::#fileList";
+    }
+
+    @GetMapping("/delete/pdf")
+    public String deletePDF(Model model, @RequestParam("file") String fileName) {
+        File dir = new File(daPdfUploadDir);
+        File files[] = dir.listFiles();
         for (File file : files) {
-            if (file.getName().contains(query)) {
+            if (file.getName().equals(fileName)) {
+                file.delete();
+            }
+        }
+
+        File filesAfterDelete[] = dir.listFiles();
+        List<String> fileNames = new ArrayList<>();
+
+        exceptNotPdfFile(filesAfterDelete, fileNames, ".pdf");
+
+        model.addAttribute("fileNames", fileNames);
+        return "home ::#fileList";
+    }
+
+    private void exceptNotPdfFile(File[] filesAfterDelete, List<String> fileNames, String s) {
+        for (File file : filesAfterDelete) {
+            if (file.getName().contains(s)) {
                 fileNames.add(file.getName());
             }
         }
-        model.addAttribute("fileNames", fileNames);
-        return "home ::#fileList";
     }
 
     @PostMapping(value = "/convert/excel", produces = "application/vnd.ms-excel")
--- src/main/resources/static/js/invoice.js
+++ src/main/resources/static/js/invoice.js
@@ -1,44 +1,5 @@
 $(document).ready(function() {
 
-    // $("#btnSubmitUploadExcel").click(function (e) {
-    //     // $("#excelUploadForm").submit();
-    //     // location.href="/convert/excel"
-    //
-    //     var form = $('#fileUploadFormLenovo')[0];
-    //     var data = new FormData(form);
-    //     data.
-    //     // var formData = new FormData($('#excelUploadForm')[0]);
-    //     // var request = new XMLHttpRequest();
-    //     // request.open("POST", "/convert/excel")
-    //     // request.send(formData);
-    // });
-
-    // $("#btnSubmitUploadExcel").click(function (e) {
-    //     //preventDefault 는 기본으로 정의된 이벤트를 작동하지 못하게 하는 메서드이다. submit을 막음
-    //     e.preventDefault();
-    //     var form = $('#excelUploadForm')[0];
-    //     var data = new FormData(form);
-    //     $.ajax({
-    //         type: 'POST',
-    //         enctype: 'multipart/form-data',
-    //         url: '/convert/excel',
-    //         data: data,
-    //         processData: false,
-    //         contentType: false,
-    //         cache: false,
-    //         success: function(data) {
-    //             // debugger;
-    //             window.open(data);
-    //             // debugger;
-    //             // var response = JSON.parse(data);
-    //             // window.location = '/Report/Download?fileGuid=' + response.FileGuid
-    //             //     + '&filename=' + response.FileName;
-    //
-    //         }
-    //     });
-    //
-    // });
-
     $("#btnSubmitPdf").click(function (e) {
         var form = $('#daPdfUploadForm')[0];
         var data = new FormData(form);
@@ -47,25 +8,20 @@ $(document).ready(function() {
         $.ajax({
             type: 'POST',
             enctype: 'multipart/form-data',
-            url: '/api/upload/pdf/da',
+            url: '/upload/pdf/da',
             data: data,
             processData: false,
             contentType: false,
             cache: false,
-            success: function(response) {
-                if (response === "success") {
-                    alert("성공적으로 업로드 되었습니다.");
-                } else {
-                    alert("업로드가 실패하였습니다.")
-                }
-            }
+        }).done(function(fragment) {
+            $("#fileList").replaceWith(fragment);
+            alert("성공적으로 업데이트 되었습니다.")
         });
         e.stopPropagation();
     });
 
     $("#searchPdfBtn").click(function() {
         var searchValue = $('#searchPdf').val();
-        $("#searchPdfBtn").prop("disabled", true);
         $.ajax({
             type: 'GET',
             enctype: 'multipart/form-data',
@@ -74,9 +30,8 @@ $(document).ready(function() {
             contentType: false,
             cache: false
         }).done(function(fragment) {
-            debugger;
             $("#fileList").replaceWith(fragment);
         });
     });
-
+    
 });
