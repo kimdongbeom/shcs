d723a032a772e623d7a40666da2666b090e25e28
dongbeom-kim <dongbeom-kim@nhnent.com>
2021-01-03 01:38:29 +0900

    PDF 미리보기 기능 작업

--- src/main/java/sinhan/custom/shcs/Controller/ViewController.java
+++ src/main/java/sinhan/custom/shcs/Controller/ViewController.java
@@ -4,7 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.compress.utils.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,16 +25,16 @@ import sinhan.custom.shcs.model.PDFExtractData;
 import sinhan.custom.shcs.model.ResultExcel;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Controller
 public class ViewController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @Value("${da.pdf.upload.dir}")
     private String daPdfUploadDir;
@@ -140,7 +144,6 @@ public class ViewController {
             FileInputStream is = new FileInputStream(daPdfUploadDir + fileName);
             IOUtils.copy(is, response.getOutputStream());
             response.setHeader("Content-Disposition", "attachment; filename=Accepted.pdf");
-            response.setCharacterEncoding("UTF-8");
             response.flushBuffer();
         } catch (IOException e) {
             e.printStackTrace();
